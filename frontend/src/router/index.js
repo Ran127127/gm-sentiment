527753485f4216df2f7029e9c3f1424411f075af
Ran127127 <butterflyandfox@163.com>
2026-08-17 21:19:45 +0800

    Add frontend/src/router/index.js

---
+++ frontend/src/router/index.js
@@ -0,0 +1,46 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import Dashboard from '../views/Dashboard.vue'
+import BrandDetail from '../views/BrandDetail.vue'
+import ArticleList from '../views/ArticleList.vue'
+import ArticleDetail from '../views/ArticleDetail.vue'
+import Recommendations from '../views/Recommendations.vue'
+
+const routes = [
+  {
+    path: '/',
+    name: 'Dashboard',
+    component: Dashboard,
+    props: true,
+  },
+  {
+    path: '/brand/:id',
+    name: 'BrandDetail',
+    component: BrandDetail,
+    props: true,
+  },
+  {
+    path: '/articles',
+    name: 'ArticleList',
+    component: ArticleList,
+    props: true,
+  },
+  {
+    path: '/articles/:id',
+    name: 'ArticleDetail',
+    component: ArticleDetail,
+    props: true,
+  },
+  {
+    path: '/recommendations',
+    name: 'Recommendations',
+    component: Recommendations,
+    props: true,
+  },
+]
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+})
+
+export default router
